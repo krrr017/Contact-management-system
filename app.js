ab789f276ddd8badc98c808b6e70f1ea66de1f7f
krrr017 <xfxrx.531@gmail.com>
2023-02-07 17:12:48 +0900

    add get

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
